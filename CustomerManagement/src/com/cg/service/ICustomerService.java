7e3b4a82cfebfc0d17656c8ea54e292423ed100b
rohitsharma45 <32483973+rohitsharma45@users.noreply.github.com>
2017-10-28 18:19:11 +0530

    Add files via upload

---
+++ CustomerManagement/src/com/cg/service/ICustomerService.java
@@ -0,0 +1,25 @@
+package com.cg.service;
+
+import java.util.List;
+
+import com.cg.model.CustomerBean;
+
+public interface ICustomerService {
+
+	CustomerBean addDetails(CustomerBean cust);
+
+	CustomerBean displayById(String custNo);
+
+	List<CustomerBean> displayAll();
+
+	void removeById(String custNo);
+
+	List<String> retrieveIds();
+
+	CustomerBean update(CustomerBean cust);
+
+
+
+	
+
+}
